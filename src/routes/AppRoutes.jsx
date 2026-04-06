6011a1f5bf66eb49ca49b0f03841c8c234e54bcc
Sumit <sumitk72004@gmail.com>
2026-04-06 09:49:20 +0530

    fix layout import case

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,8 +3,7 @@ import Home from "../pages/Home";
 import About from "../pages/About";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-
-import MainLayout from "../layouts/MAinLayout";
+import MainLayout from "../layouts/MainLayout";
 import ProtectedDashboard from "./ProtectedDashboard";
 import AuthProtected from "./AuthProtected";
 import Products from "../pages/Products";
